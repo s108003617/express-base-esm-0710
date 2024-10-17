12ca8b553101bc50ac7c571b83d91ada3f46ef6e
en2000 <162282381+s108003617@users.noreply.github.com>
2024-10-17 08:41:14 +0800

    port

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
